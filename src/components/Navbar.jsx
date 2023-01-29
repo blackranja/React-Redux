2833925bb5f4667ac0c0767f73ada0ad9faf11a0
Josphat karanja <josphatranja243@gmail.com>
2023-01-29 18:39:54 +0300

    Navbar Template

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,26 @@ import React, { useState } from 'react';
 import { AiOutlineClose } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
 import logo from "../logo.svg";
-import NavbarItems from './NavbarItems';
 
+//const title = "RanjaRedux";
+const NavbarItems = ({ title, classProps }) => (
+    <li className={`mx-4 cursor-pointer ${classProps}`}>
+        {title}
+    </li>
+);
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <nav className="w-full flex md:justigy-center justify-between items-center p-4">
+    <nav className="w-full flex md:justigy-center justify-between items-center p-4 gradient-bg-welcome">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+        <img src={logo} alt="logo" width="25" className="cursor-pointer" />
         
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initail">
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
-          <NavbarItems key={item + index} title={title} />
+          <NavbarItems key={item + index} title={item} />
         ))}
-        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-ponter hover:bg-[#2546bd]">
+        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
           Login
         </li>
       </ul>
@@ -25,7 +30,7 @@ const Navbar = () => {
           <GiHamburgerMenu fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>
         )}
         {toggleMenu && (
-          <ul className="z-10 fixed -top-0 -right-2 p-3 w-[70vm] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
+          <ul className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
             <li className="text-xl w-full my-2">
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
               
--- src/components/NavbarItems.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const NavbarItems = ({ title, classProps }) => (
-    <li className={`mx-4 cursor-pointer ${classProps}`}>
-        {title}
-    </li>
-);
-export default NavbarItems;
