809c32ee80d7b16d9a85723702e1ef7a3ba73d11
Josphat karanja <josphatranja243@gmail.com>
2023-01-29 17:23:41 +0300

    NavBar Template

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,16 @@ const Navbar = () => {
         )}
         {toggleMenu && (
           <ul className="z-10 fixed -top-0 -right-2 p-3 w-[70vm] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
-            
+            <li className="text-xl w-full my-2">
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
+              
+            </li>
+            {
+              ["Market", "Economy", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                <NavbarItems key={item + index} title={item} classProps="my-2 text-lg"/>
+              ))
+            }
+
           </ul>
         )}
       </div>
