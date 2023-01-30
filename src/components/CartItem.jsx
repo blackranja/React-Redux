7c979ae7bb8a49d3a3619442fa7dad42aef0eaba
Josphat karanja <josphatranja243@gmail.com>
2023-01-30 11:18:46 +0300

    Redux Shopping Cart

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
+import { removeItem } from '../features/cart/cartSlice';
+import { useDispatch } from 'react-redux';
 
 const CartItem = ({ id, img, title, price, amount }) => {
+    const dispatch = useDispatch();
     
   return (
       <div className="w-full flex justify-between">
@@ -11,14 +14,22 @@ const CartItem = ({ id, img, title, price, amount }) => {
               <div className="text-[#ffffff] w-full items-center justify-between">
                   <h4 className="font-bold text-gradient px-4">{title}</h4>
                   <h4 className="font-bold text-gradient px-4">${price}</h4>
-                  <button className="rounded-md m-2 p-2 bg-[#ffccffcc] text-[#ffffff]">remove</button>
+              <button
+                  onClick={() => {
+                      dispatch(removeItem(id));
+                  }}
+                  className="rounded-md m-2 p-2 bg-[#ffccffcc] text-[#ffffff]">
+                  remove
+              </button>
               </div>
               <div className="text-[#ffffff] w-full items-center justify-between">
-                  <button className="rounded-md m-2 p-2 bg-[#ffccffcc] text-[#ffffff]">
+              <button
+                  className="rounded-md m-2 p-2 bg-[#ffccffcc] text-[#ffffff]">
                       <FaChevronUp/>
                   </button>
                   <p className="">{amount}</p>
-                  <button className="rounded-md m-2 p-2 bg-[#ffccffcc] text-[#ffffff]">
+              <button
+                  className="rounded-md m-2 p-2 bg-[#ffccffcc] text-[#ffffff]">
                       <FaChevronDown/>
                   </button>
               </div>
